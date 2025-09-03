79f927f711effdaecdd1e073f74c55d2dc550442
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-03 03:31:10 +0000

    container config done

---
+++ o1_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_Id=144556
+let accountEmail="chhavi@gmail.com" /*only used let and const */
+var accountPassword ="12345" /*not to use the var due to problem of scope .*/
+accountCity="Jaipur"
+
+// account_Id=2 //not allowed
+accountEmail="tiya@gmail.com"
+accountPassword=123456329
+accountCity="delhi"
+let accountState
+
+
+console.log(account_Id);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountEmail,accountPassword,account_Id,accountCity,accountState])
