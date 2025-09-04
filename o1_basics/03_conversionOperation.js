45c842a412d0f6c4724b1b7340ef05752b9e397c
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-04 13:57:07 +0000

    Conversion of datatype

---
+++ o1_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score ="dlglr"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"-> 33
+//"33sfe"->NaN
+//true->1,false->0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1->true
+//""->false
+//"vnlfdng"->true
+
+let somenumber =33
+let stringNumber =String(somenumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
