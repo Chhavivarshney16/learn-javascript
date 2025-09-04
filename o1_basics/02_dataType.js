bac3fcc654a2aa33416ca77713243ca3084fe2cb
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-04 13:15:56 +0000

    datatype

---
+++ o1_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict";  //treate all js code as newer version
+//alert(3+3) //we are using node js not browser 
+console.log(3+3);
+console.log("chhavi")
+
+let name="chhavi"
+let age =15
+let isLoggedIn=false
+
+//number ->2to power 53
+//bigint 
+//string -> ""
+//boolean ->true/false
+//null->standalone value
+//undefined->not defined 
+//symbol ->unique
+
+//object 
+
+console.log(typeof undefined)//undefined
+console.log(typeof null) //object
