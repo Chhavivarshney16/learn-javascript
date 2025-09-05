dc2140db62ab1df8c95cebf26705b93e514b612a
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-05 03:44:45 +0000

    compare

--- o1_basics/03_conversionOperation.js
+++ o1_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score ="dlglr"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber =Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 //"33"-> 33
 //"33sfe"->NaN
@@ -13,7 +13,7 @@ console.log(valueInNumber)
 
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1->true
 //""->false
@@ -21,5 +21,47 @@ console.log(booleanIsLoggedIn)
 
 let somenumber =33
 let stringNumber =String(somenumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// **************operator******************
+let value=2
+let negValue=-value
+// console.log(negValue)
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3/3)
+// console.log(3%3)
+// console.log(3**3)
+
+let str1="hello"
+let str2=" chhavi"
+
+let str3=str1+str2
+// console.log(str3)
+
+// console.log(3+"2")
+// console.log("3"+2)
+// console.log("3"+"2")
+// console.log("3"+2+2)
+// console.log(3+2+"1"+"2"+4)
+// console.log(3+2+"1")
+// console.log(+true)
+// console.log(+"")
+
+let num1,num2,num3
+
+num1=num2=num3=2+2
+
+// let gamecounter1=100
+// gamecounter1++
+// // ++gamecounter1
+// console.log(gamecounter1)
+
+let x=3
+let y=x++
+console.log(`x:${x}, y: ${y}`)
+
+
