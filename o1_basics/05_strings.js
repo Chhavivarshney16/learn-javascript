22bf24e14750d927d0b76c43a9a2bf8707e2782f
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-06 09:02:27 +0000

    String

---
+++ o1_basics/05_strings.js
@@ -0,0 +1,40 @@
+const name ="chhavi"
+const repocount=50
+
+// console.log(name+ repocount+" Value");
+// outdated this syntax
+
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`)
+
+const gameName=new String("chhavi varshney")
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(7));
+// console.log(gameName.indexOf("a"));
+
+const newString= gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString =gameName.slice(-5,13)
+console.log(anotherString);
+
+const newStringOne ="      vars hney     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="http.//chhavi.com/chhavi16varshney"
+const news=url.replace("16","-")
+console.log(news
+);
+
+console.log(url.includes('88'));
+console.log(url.split("/"));
+
+
+
+
+
+
