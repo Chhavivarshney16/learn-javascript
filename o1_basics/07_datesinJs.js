e2343f9f9b7d4c3f4575b4652e76369e1b369ffb
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-06 09:31:20 +0000

    dates and time

---
+++ o1_basics/07_datesinJs.js
@@ -0,0 +1,21 @@
+// Dates
+
+let mydate=new Date()
+// console.log(mydate);
+
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleString());
+// console.log(typeof mydate);
+
+let myCreteDate=new Date(2023,0,23,5,3)
+//  console.log(myCreteDate.toLocaleDateString());
+let myDate1=new Date("2023-01-14")
+// console.log(myDate1);
+
+let myTimeStamp=Date.now()
+console.log(myTimeStamp);
+console.log(myCreteDate.getTime());
+
+
