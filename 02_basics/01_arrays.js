57694cd824d8a932fdeb108efef22c2b8a403b52
Chhavivarshney16 <varshneychhavi1616@gmail.com>
2025-09-07 08:59:16 +0000

    array basics1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,45 @@
+// array
+
+const myArr =[0,1,2,3,4,5]
+const myHeroes=["chhavi","umang","tiya"]
+
+// const myArray2=new Array(1,2,3,4,5)
+// console.log(myArr[3]);
+
+// Array methods:
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+// console.log(myArr);
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+const newArr=myArr.join()
+// console.log(myArr);
+// console.log(myArr);
+// console.log(typeof myArr);
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// ....slice and splice
+console.log("A",myArr);
+
+const narr1=myArr.slice(0,3)
+console.log(narr1);
+console.log("B",myArr);
+
+const narr2= myArr.splice(0,3)
+console.log(narr2);
+console.log("C",myArr);
+
+
+
+
+
+
+
